9607de2ccfdd08d7b7ce5a285f38506d71fd9c1e
Dacuna <dacuna@ucm.es>
2018-12-12 18:13:46 +0100

    initial state

--- Practica Guiada JQuery/index.js
+++ Practica Guiada JQuery/index.js
@@ -1,36 +1,7 @@
 "use strict"
-/*function insertMatrix(selector, matriz){
-    $(selector).append(matriz);
-}*/
-function insertMatrix(selector, matriz, callback) {
-    $(selector).append(matriz);
-    $("table").on("click", "td", callback);
-}
 
-function createMatrix(matriz) {
-    let new_matrix = $("<table></table>");
-    matriz.forEach(row => {
-        let tr = $("<tr></tr>");
-        row.forEach(element => {
-            let new_elem = $("<td></td>");
-            new_elem.text(element);
-            tr.append(new_elem);
-        })
-        new_matrix.append(tr);
-    });
-    return new_matrix;
-}
 
 
 $(function () {
-    let m = [
-        ["Esto", "es", "una fila"],
-        ["aquí", "va", "otra fila"],
-        ["y", "aquí", "otra más"]
-    ];
-    let matriz = createMatrix(m);
-    insertMatrix("div", matriz, (event) => {
-        let elementoPulsado = $(event.target);
-        alert("Has hecho clic en " + elementoPulsado.text());
-    });
+   
 });
