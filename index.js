ac081a4b0c188d76cde0175137b185f5fbea0a00
ZReKoJ <zhong.9745@gmail.com>
2018-12-12 19:52:33 +0100

    flex cards

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 "use strict"
 
-
-
-$(function () {
-   
+$(() => {
+   new SearchPairs(5, 4);
 });
+
+class SearchPairs {
+    constructor(_rows, _cols) {
+        this.rows = _rows;
+        this.cols = _cols;
+        this.matrix = Array(this.rows).fill(Array(this.cols).fill(undefined));
+        this.fillMatrix();
+    } 
+
+    fillMatrix() {
+        let values = Array(this.rows * this.cols).fill().map((element, index) => (index - (index % 2)) / 2);
+        values
+        console.log(values);
+        let counter;
+        for (let i = 0; i < this.rows; ++i){
+            this.matrix.push([]);
+            for (let j = 0; j < this.cols; ++j){
+                counter = (i * this.cols) + j;
+
+            }
+        }
+    }
+}
