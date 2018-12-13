867b1658417f7e5d03fee2120819326aa78af72c
ZReKoJ <zhong.9745@gmail.com>
2018-12-13 17:15:16 +0100

    logic done

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 "use strict"
 
 $(() => {
-   new SearchPairs(5, 4);
+    var game = new SearchPairs(5, 4);
 });
 
 class SearchPairs {
     constructor(_rows, _cols) {
         this.rows = _rows;
         this.cols = _cols;
-        this.matrix = Array(this.rows).fill(Array(this.cols).fill(undefined));
+
+        this.point = undefined;
+
+        this.matrix = Array(this.rows).fill().map(element => Array(this.cols).fill(undefined));
         this.fillMatrix();
-    } 
+    }
 
     fillMatrix() {
         let values = Array(this.rows * this.cols).fill().map((element, index) => (index - (index % 2)) / 2);
-        values
-        console.log(values);
-        let counter;
-        for (let i = 0; i < this.rows; ++i){
-            this.matrix.push([]);
-            for (let j = 0; j < this.cols; ++j){
-                counter = (i * this.cols) + j;
-
+        for (let i = 0; i < this.rows; ++i) {
+            for (let j = 0; j < this.cols; ++j) {
+                let number = Math.floor(Math.random() * values.length);
+                this.matrix[i][j] = values[number];
+                values.splice(number, 1);
             }
         }
+    }
+
+    clickOn(x, y) {
+        let point = new Point(x, y);
+        if (this.point == undefined) {
+            this.point = point;
+        } else {
+            check(point);
+            this.point = undefined;
+        }
+    }
+
+    check(point) {
+        if (this.matrix[this.point.x][this.point.y] == this.matrix[point.x][point.y]) {
+            this.matrix[this.point.x][this.point.y] = undefined;
+            this.matrix[point.x][point.y] = undefined;
+        }
+    }
+}
+
+class Point {
+    constructor(_x, _y) {
+        this.x = _x;
+        this.y = _y;
     }
 }
